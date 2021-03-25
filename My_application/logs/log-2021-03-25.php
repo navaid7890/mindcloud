bdbae75ac9a1bb2e4c65c2f66325bb43f694ac71
talhaMG <talha@manageglobally.io>
2021-03-25 13:21:25 +0500

    rool builder form

--- My_application/logs/log-2021-03-25.php
+++ My_application/logs/log-2021-03-25.php
@@ -17,3 +17,35 @@ ERROR - 2021-03-25 05:55:14 --> 404 Page Not Found: Profile/profile.png
 ERROR - 2021-03-25 07:19:35 --> 404 Page Not Found: Profile/profile.png
 ERROR - 2021-03-25 07:23:09 --> 404 Page Not Found: Footerlogopng/index
 ERROR - 2021-03-25 07:36:33 --> 404 Page Not Found: Footerlogopng/index
+ERROR - 2021-03-25 08:56:40 --> 404 Page Not Found: account/Profile/movie.mp4
+ERROR - 2021-03-25 08:56:40 --> 404 Page Not Found: account/Profile/movie.ogg
+ERROR - 2021-03-25 08:57:41 --> 404 Page Not Found: account/Profile/movie.mp4
+ERROR - 2021-03-25 08:57:41 --> 404 Page Not Found: account/Profile/movie.ogg
+ERROR - 2021-03-25 08:58:05 --> 404 Page Not Found: account/Profile/movie.mp4
+ERROR - 2021-03-25 08:58:06 --> 404 Page Not Found: account/Profile/movie.ogg
+ERROR - 2021-03-25 09:19:34 --> 404 Page Not Found: Profile/profile.png
+ERROR - 2021-03-25 09:20:04 --> 404 Page Not Found: admin/Profile/profile.png
+ERROR - 2021-03-25 09:20:07 --> 404 Page Not Found: admin/Client/profile
+ERROR - 2021-03-25 13:20:30 --> Severity: Warning --> Illegal string offset 'form_data' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 395
+ERROR - 2021-03-25 13:20:30 --> Severity: Warning --> Illegal string offset 'user_input' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 397
+ERROR - 2021-03-25 13:20:30 --> Severity: Warning --> Illegal string offset 'page_title_min' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 443
+ERROR - 2021-03-25 13:20:30 --> Severity: Warning --> Illegal string offset 'page_title' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 444
+ERROR - 2021-03-25 13:20:30 --> Severity: Warning --> Illegal string offset 'class_name' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 445
+ERROR - 2021-03-25 13:20:30 --> Severity: Warning --> Illegal string offset 'model_name' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 446
+ERROR - 2021-03-25 13:20:30 --> Severity: Warning --> Illegal string offset 'model_obj' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 447
+ERROR - 2021-03-25 13:20:30 --> Severity: 4096 --> Object of class Model_client could not be converted to string /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 447
+ERROR - 2021-03-25 13:20:30 --> Severity: Warning --> Illegal string offset 'form_fields' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 448
+ERROR - 2021-03-25 13:20:30 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /home/talha/www/cloud/My_system/core/Exceptions.php:271) /home/talha/www/cloud/My_system/core/Common.php 570
+ERROR - 2021-03-25 13:20:30 --> Severity: Error --> Cannot use string offset as an array /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 448
+ERROR - 2021-03-25 13:20:46 --> Severity: Warning --> Illegal string offset 'form_data' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 395
+ERROR - 2021-03-25 13:20:46 --> Severity: Warning --> Illegal string offset 'user_input' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 397
+ERROR - 2021-03-25 13:20:46 --> Severity: Warning --> Illegal string offset 'page_title_min' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 443
+ERROR - 2021-03-25 13:20:46 --> Severity: Warning --> Illegal string offset 'page_title' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 444
+ERROR - 2021-03-25 13:20:46 --> Severity: Warning --> Illegal string offset 'class_name' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 445
+ERROR - 2021-03-25 13:20:46 --> Severity: Warning --> Illegal string offset 'model_name' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 446
+ERROR - 2021-03-25 13:20:46 --> Severity: Warning --> Illegal string offset 'model_obj' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 447
+ERROR - 2021-03-25 13:20:46 --> Severity: 4096 --> Object of class Model_client could not be converted to string /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 447
+ERROR - 2021-03-25 13:20:46 --> Severity: Warning --> Illegal string offset 'form_fields' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 448
+ERROR - 2021-03-25 13:20:46 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /home/talha/www/cloud/My_system/core/Exceptions.php:271) /home/talha/www/cloud/My_system/core/Common.php 570
+ERROR - 2021-03-25 13:20:46 --> Severity: Error --> Cannot use string offset as an array /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 448
+ERROR - 2021-03-25 09:20:49 --> 404 Page Not Found: admin/Profile/profile.png
--- My_application/views/account/account_area_theme/tools.php
+++ My_application/views/account/account_area_theme/tools.php
@@ -75,9 +75,60 @@
                                     <div class="box-1 showfirst" style="">      
                                     <form id="regForm" action="">
                                           
-                                          <div id="radio1" class="multi-fld">
-                                          <? $this->load->view("widgets/my");?>
-                                          </div>
+                                    <div class="tab">
+                                       <div class="fld-textarea">
+                                       <textarea placeholder="First name..." oninput="this.className = ''"></textarea>
+                                       </div>  
+                                    </div>
+
+                                    <div class="tab">
+                                       <div class="fld-text">
+                                          <input type="email" placeholder="E-mail..." oninput="this.className = ''">
+                                       </div>
+
+                                       <div class="fld-text">
+                                          <input type="number" placeholder="Phone..." oninput="this.className = ''">
+                                       </div>
+                                    </div>
+
+                                    <div class="tab">
+                                       <div class="fld-text">
+                                       <input type="text" placeholder="Username..." oninput="this.className = ''">
+                                       </div>
+
+                                       <div class="fld-text">
+                                       <input type="text" placeholder="Postal Code" oninput="this.className = ''">
+                                       </div>
+                                    </div>
+
+                                    <div class="tab">
+                                       <div class="fld-text">
+                                       <input type="text" placeholder="First name..." oninput="this.className = ''">
+                                       </div>  
+                                       <div class="fld-text">
+                                       <input type="text" placeholder="First name..." oninput="this.className = ''">
+                                       </div>  
+                                    </div>
+
+                                    <div class="tab">
+                                       <div class="fld-text">
+                                          <input type="email" placeholder="E-mail..." oninput="this.className = ''">
+                                       </div>
+
+                                       <div class="fld-text">
+                                          <input type="number" placeholder="Phone..." oninput="this.className = ''">
+                                       </div>
+                                    </div>
+
+                                    <div class="tab">
+                                       <div class="fld-text">
+                                       <input type="text" placeholder="Username..." oninput="this.className = ''">
+                                       </div>
+
+                                       <div class="fld-text">
+                                       <input type="text" placeholder="Postal Code" oninput="this.className = ''">
+                                       </div>
+                                    </div>
 
                           
                                           <div style="overflow:auto;">
