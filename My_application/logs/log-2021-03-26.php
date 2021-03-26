4aa7bb3c4bf3e3698f59ef16c80bb432f5d07294
talhaMG <talha@manageglobally.io>
2021-03-26 10:35:21 +0500

    widgets of home page

---
+++ My_application/logs/log-2021-03-26.php
@@ -0,0 +1,28 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-03-26 06:33:10 --> 404 Page Not Found: Profile/profile.png
+ERROR - 2021-03-26 06:33:12 --> 404 Page Not Found: admin/Profile/profile.png
+ERROR - 2021-03-26 10:33:13 --> Severity: Warning --> mysqli_connect(): Server sent charset (255) unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-03-26 10:33:13 --> Severity: Warning --> mysqli_connect(): (HY000/2054): Server sent charset unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-03-26 10:33:13 --> Severity: Warning --> mysqli_real_escape_string() expects parameter 1 to be mysqli, boolean given /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 202
+ERROR - 2021-03-26 10:33:14 --> Severity: Warning --> mysqli_connect(): Server sent charset (255) unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-03-26 10:33:14 --> Severity: Warning --> mysqli_connect(): (HY000/2054): Server sent charset unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-03-26 10:33:14 --> Severity: Warning --> mysqli_real_escape_string() expects parameter 1 to be mysqli, boolean given /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 202
+ERROR - 2021-03-26 10:33:16 --> Severity: Warning --> mysqli_connect(): Server sent charset (255) unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-03-26 10:33:16 --> Severity: Warning --> mysqli_connect(): (HY000/2054): Server sent charset unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-03-26 10:33:16 --> Severity: Warning --> mysqli_real_escape_string() expects parameter 1 to be mysqli, boolean given /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 202
+ERROR - 2021-03-26 10:33:16 --> Severity: Warning --> mysqli_connect(): Server sent charset (255) unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-03-26 10:33:16 --> Severity: Warning --> mysqli_connect(): (HY000/2054): Server sent charset unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-03-26 10:33:16 --> Severity: Warning --> mysqli_real_escape_string() expects parameter 1 to be mysqli, boolean given /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 202
+ERROR - 2021-03-26 10:33:17 --> Severity: Warning --> mysqli_connect(): Server sent charset (255) unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-03-26 10:33:17 --> Severity: Warning --> mysqli_connect(): (HY000/2054): Server sent charset unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-03-26 10:33:17 --> Severity: Warning --> mysqli_real_escape_string() expects parameter 1 to be mysqli, boolean given /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 202
+ERROR - 2021-03-26 10:33:18 --> Severity: Warning --> mysqli_connect(): Server sent charset (255) unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-03-26 10:33:18 --> Severity: Warning --> mysqli_connect(): (HY000/2054): Server sent charset unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-03-26 10:33:18 --> Severity: Warning --> mysqli_real_escape_string() expects parameter 1 to be mysqli, boolean given /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 202
+ERROR - 2021-03-26 10:33:19 --> Severity: Warning --> mysqli_connect(): Server sent charset (255) unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-03-26 10:33:19 --> Severity: Warning --> mysqli_connect(): (HY000/2054): Server sent charset unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-03-26 10:33:19 --> Severity: Warning --> mysqli_real_escape_string() expects parameter 1 to be mysqli, boolean given /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 202
+ERROR - 2021-03-26 10:33:20 --> Severity: Warning --> mysqli_connect(): Server sent charset (255) unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-03-26 10:33:20 --> Severity: Warning --> mysqli_connect(): (HY000/2054): Server sent charset unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-03-26 10:33:20 --> Severity: Warning --> mysqli_real_escape_string() expects parameter 1 to be mysqli, boolean given /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 202
---
+++ My_application/views/widgets/_most_popular_categories.php
@@ -0,0 +1,20 @@
+<div class="mostcateSec">
+    <div class="mostHead">
+        <h2>Most Popular <strong> Categories </strong></h2>
+    </div>
+    <div class="space"><br><br></div>
+    
+    <ul class="cate-list">
+        <li><p><span><img src="<?=i('')?>icons/category/1.svg"></span> Business Model (4)</p></li>
+        <li><p><span><img src="<?=i('')?>icons/category/1.svg"></span> Digital marketing (12)</p></li>
+        <li><p><span><img src="<?=i('')?>icons/category/1.svg"></span> Finance and Valuation (6)</p></li>
+        <li><p><span><img src="<?=i('')?>icons/category/1.svg"></span> Leadership (9)</p></li>
+        <li><p><span><img src="<?=i('')?>icons/category/1.svg"></span> Human resources (14)</p></li>
+        <li><p><span><img src="<?=i('')?>icons/category/1.svg"></span> Customer Acquisition (12)</p></li>
+        <li><p><span><img src="<?=i('')?>icons/category/1.svg"></span> Marketing & Sales (10)</p></li>
+        <li><p><span><img src="<?=i('')?>icons/category/1.svg"></span> Branding (4)</p></li>
+        <li><p><span><img src="<?=i('')?>icons/category/1.svg"></span> Business Model (4)</p></li>
+    </ul>
+    
+    <a href="#" class="btn-theme btn-hover">View all Tutorials <span></span></a>
+</div>
