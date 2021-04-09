7b522f6e7daea6e8bf0d9617e6fb197e0b809cd4
talhaMG <talha@manageglobally.io>
2021-04-09 16:25:03 +0500

    htaccess code added

--- My_application/logs/log-2021-04-09.php
+++ My_application/logs/log-2021-04-09.php
@@ -35,3 +35,75 @@ ERROR - 2021-04-09 16:14:32 --> Severity: 4096 --> Object of class Model_course 
 ERROR - 2021-04-09 16:14:32 --> Severity: Warning --> Illegal string offset 'form_fields' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 448
 ERROR - 2021-04-09 16:14:32 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /home/talha/www/cloud/My_system/core/Exceptions.php:271) /home/talha/www/cloud/My_system/core/Common.php 570
 ERROR - 2021-04-09 16:14:32 --> Severity: Error --> Cannot use string offset as an array /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 448
+ERROR - 2021-04-09 13:23:28 --> 404 Page Not Found: admin/Profile/profile.png
+ERROR - 2021-04-09 16:23:29 --> Severity: Warning --> mysqli_connect(): Server sent charset (255) unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:29 --> Severity: Warning --> mysqli_connect(): (HY000/2054): Server sent charset unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:29 --> Severity: Warning --> mysqli_real_escape_string() expects parameter 1 to be mysqli, boolean given /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 202
+ERROR - 2021-04-09 16:23:30 --> Severity: Warning --> mysqli_connect(): Server sent charset (255) unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:30 --> Severity: Warning --> mysqli_connect(): (HY000/2054): Server sent charset unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:30 --> Severity: Warning --> mysqli_real_escape_string() expects parameter 1 to be mysqli, boolean given /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 202
+ERROR - 2021-04-09 16:23:31 --> Severity: Warning --> mysqli_connect(): Server sent charset (255) unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:31 --> Severity: Warning --> mysqli_connect(): (HY000/2054): Server sent charset unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:31 --> Severity: Warning --> mysqli_real_escape_string() expects parameter 1 to be mysqli, boolean given /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 202
+ERROR - 2021-04-09 16:23:31 --> Severity: Warning --> mysqli_connect(): Server sent charset (255) unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:31 --> Severity: Warning --> mysqli_connect(): (HY000/2054): Server sent charset unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:31 --> Severity: Warning --> mysqli_real_escape_string() expects parameter 1 to be mysqli, boolean given /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 202
+ERROR - 2021-04-09 16:23:32 --> Severity: Warning --> mysqli_connect(): Server sent charset (255) unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:32 --> Severity: Warning --> mysqli_connect(): (HY000/2054): Server sent charset unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:32 --> Severity: Warning --> mysqli_real_escape_string() expects parameter 1 to be mysqli, boolean given /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 202
+ERROR - 2021-04-09 16:23:32 --> Severity: Warning --> mysqli_connect(): Server sent charset (255) unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:32 --> Severity: Warning --> mysqli_connect(): (HY000/2054): Server sent charset unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:32 --> Severity: Warning --> mysqli_real_escape_string() expects parameter 1 to be mysqli, boolean given /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 202
+ERROR - 2021-04-09 16:23:33 --> Severity: Warning --> mysqli_connect(): Server sent charset (255) unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:33 --> Severity: Warning --> mysqli_connect(): (HY000/2054): Server sent charset unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:33 --> Severity: Warning --> mysqli_real_escape_string() expects parameter 1 to be mysqli, boolean given /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 202
+ERROR - 2021-04-09 16:23:34 --> Severity: Warning --> mysqli_connect(): Server sent charset (255) unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:34 --> Severity: Warning --> mysqli_connect(): (HY000/2054): Server sent charset unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:34 --> Severity: Warning --> mysqli_real_escape_string() expects parameter 1 to be mysqli, boolean given /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 202
+ERROR - 2021-04-09 16:23:34 --> Severity: Warning --> mysqli_connect(): Server sent charset (255) unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:34 --> Severity: Warning --> mysqli_connect(): (HY000/2054): Server sent charset unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:34 --> Severity: Warning --> mysqli_real_escape_string() expects parameter 1 to be mysqli, boolean given /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 202
+ERROR - 2021-04-09 16:23:35 --> Severity: Warning --> mysqli_connect(): Server sent charset (255) unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:35 --> Severity: Warning --> mysqli_connect(): (HY000/2054): Server sent charset unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:35 --> Severity: Warning --> mysqli_real_escape_string() expects parameter 1 to be mysqli, boolean given /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 202
+ERROR - 2021-04-09 16:23:36 --> Severity: Warning --> mysqli_connect(): Server sent charset (255) unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:36 --> Severity: Warning --> mysqli_connect(): (HY000/2054): Server sent charset unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:36 --> Severity: Warning --> mysqli_real_escape_string() expects parameter 1 to be mysqli, boolean given /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 202
+ERROR - 2021-04-09 16:23:37 --> Severity: Warning --> mysqli_connect(): Server sent charset (255) unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:37 --> Severity: Warning --> mysqli_connect(): (HY000/2054): Server sent charset unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:37 --> Severity: Warning --> mysqli_real_escape_string() expects parameter 1 to be mysqli, boolean given /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 202
+ERROR - 2021-04-09 16:23:37 --> Severity: Warning --> mysqli_connect(): Server sent charset (255) unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:37 --> Severity: Warning --> mysqli_connect(): (HY000/2054): Server sent charset unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:37 --> Severity: Warning --> mysqli_real_escape_string() expects parameter 1 to be mysqli, boolean given /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 202
+ERROR - 2021-04-09 16:23:38 --> Severity: Warning --> mysqli_connect(): Server sent charset (255) unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:38 --> Severity: Warning --> mysqli_connect(): (HY000/2054): Server sent charset unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:38 --> Severity: Warning --> mysqli_real_escape_string() expects parameter 1 to be mysqli, boolean given /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 202
+ERROR - 2021-04-09 16:23:39 --> Severity: Warning --> mysqli_connect(): Server sent charset (255) unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:39 --> Severity: Warning --> mysqli_connect(): (HY000/2054): Server sent charset unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:39 --> Severity: Warning --> mysqli_real_escape_string() expects parameter 1 to be mysqli, boolean given /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 202
+ERROR - 2021-04-09 16:23:40 --> Severity: Warning --> mysqli_connect(): Server sent charset (255) unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:40 --> Severity: Warning --> mysqli_connect(): (HY000/2054): Server sent charset unknown to the client. Please, report to the developers /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 166
+ERROR - 2021-04-09 16:23:40 --> Severity: Warning --> mysqli_real_escape_string() expects parameter 1 to be mysqli, boolean given /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 202
+ERROR - 2021-04-09 13:23:59 --> 404 Page Not Found: admin/Profile/profile.png
+ERROR - 2021-04-09 16:24:03 --> Severity: Warning --> Illegal string offset 'form_data' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 395
+ERROR - 2021-04-09 16:24:03 --> Severity: Warning --> Illegal string offset 'user_input' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 397
+ERROR - 2021-04-09 16:24:03 --> Severity: Warning --> Illegal string offset 'page_title_min' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 443
+ERROR - 2021-04-09 16:24:03 --> Severity: Warning --> Illegal string offset 'page_title' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 444
+ERROR - 2021-04-09 16:24:03 --> Severity: Warning --> Illegal string offset 'class_name' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 445
+ERROR - 2021-04-09 16:24:03 --> Severity: Warning --> Illegal string offset 'model_name' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 446
+ERROR - 2021-04-09 16:24:03 --> Severity: Warning --> Illegal string offset 'model_obj' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 447
+ERROR - 2021-04-09 16:24:03 --> Severity: 4096 --> Object of class Model_course could not be converted to string /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 447
+ERROR - 2021-04-09 16:24:03 --> Severity: Warning --> Illegal string offset 'form_fields' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 448
+ERROR - 2021-04-09 16:24:03 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /home/talha/www/cloud/My_system/core/Exceptions.php:271) /home/talha/www/cloud/My_system/core/Common.php 570
+ERROR - 2021-04-09 16:24:03 --> Severity: Error --> Cannot use string offset as an array /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 448
+ERROR - 2021-04-09 16:24:33 --> Severity: Warning --> Illegal string offset 'form_data' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 395
+ERROR - 2021-04-09 16:24:33 --> Severity: Warning --> Illegal string offset 'user_input' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 397
+ERROR - 2021-04-09 16:24:33 --> Severity: Warning --> Illegal string offset 'page_title_min' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 443
+ERROR - 2021-04-09 16:24:33 --> Severity: Warning --> Illegal string offset 'page_title' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 444
+ERROR - 2021-04-09 16:24:33 --> Severity: Warning --> Illegal string offset 'class_name' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 445
+ERROR - 2021-04-09 16:24:33 --> Severity: Warning --> Illegal string offset 'model_name' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 446
+ERROR - 2021-04-09 16:24:33 --> Severity: Warning --> Illegal string offset 'model_obj' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 447
+ERROR - 2021-04-09 16:24:33 --> Severity: 4096 --> Object of class Model_course could not be converted to string /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 447
+ERROR - 2021-04-09 16:24:33 --> Severity: Warning --> Illegal string offset 'form_fields' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 448
+ERROR - 2021-04-09 16:24:33 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /home/talha/www/cloud/My_system/core/Exceptions.php:271) /home/talha/www/cloud/My_system/core/Common.php 570
+ERROR - 2021-04-09 16:24:33 --> Severity: Error --> Cannot use string offset as an array /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 448
