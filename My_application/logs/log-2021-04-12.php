8838ea0110af6076673235ff075ed38f79b1a1fb
talhaMG <talha@manageglobally.io>
2021-04-12 16:42:16 +0500

    test

--- My_application/logs/log-2021-04-12.php
+++ My_application/logs/log-2021-04-12.php
@@ -186,3 +186,14 @@ ERROR - 2021-04-12 16:06:19 --> Severity: Warning --> Cannot modify header infor
 ERROR - 2021-04-12 16:06:19 --> Severity: Error --> Cannot use string offset as an array /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 451
 ERROR - 2021-04-12 13:10:14 --> 404 Page Not Found: admin/Profile/profile.png
 ERROR - 2021-04-12 13:10:17 --> 404 Page Not Found: admin/Course/profile
+ERROR - 2021-04-12 16:28:27 --> Severity: Warning --> Illegal string offset 'form_data' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 397
+ERROR - 2021-04-12 16:28:27 --> Severity: Warning --> Illegal string offset 'user_input' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 399
+ERROR - 2021-04-12 16:28:27 --> Severity: Warning --> Illegal string offset 'page_title_min' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 446
+ERROR - 2021-04-12 16:28:27 --> Severity: Warning --> Illegal string offset 'page_title' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 447
+ERROR - 2021-04-12 16:28:27 --> Severity: Warning --> Illegal string offset 'class_name' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 448
+ERROR - 2021-04-12 16:28:27 --> Severity: Warning --> Illegal string offset 'model_name' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 449
+ERROR - 2021-04-12 16:28:27 --> Severity: Warning --> Illegal string offset 'model_obj' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 450
+ERROR - 2021-04-12 16:28:27 --> Severity: 4096 --> Object of class Model_course could not be converted to string /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 450
+ERROR - 2021-04-12 16:28:27 --> Severity: Warning --> Illegal string offset 'form_fields' /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 451
+ERROR - 2021-04-12 16:28:27 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /home/talha/www/cloud/My_system/core/Exceptions.php:271) /home/talha/www/cloud/My_system/core/Common.php 570
+ERROR - 2021-04-12 16:28:27 --> Severity: Error --> Cannot use string offset as an array /home/talha/www/cloud/My_application/core/MY_Controller_Admin.php 451
