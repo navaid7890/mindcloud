c8390f9c66032b6d86ec9e52df40a01c5e38be90
talhaMG <talha@manageglobally.io>
2021-04-22 10:45:09 +0500

    business model tool builder view settled

---
+++ My_application/logs/log-2021-04-22.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-04-22 06:51:40 --> 404 Page Not Found: account/Profile/assets
+ERROR - 2021-04-22 07:08:38 --> 404 Page Not Found: account/Profile/assets
+ERROR - 2021-04-22 07:15:05 --> 404 Page Not Found: Profile/profile.png
+ERROR - 2021-04-22 07:15:08 --> 404 Page Not Found: admin/Profile/profile.png
+ERROR - 2021-04-22 07:40:15 --> 404 Page Not Found: Profile/profile.png
+ERROR - 2021-04-22 07:40:19 --> 404 Page Not Found: Accout_area/index
+ERROR - 2021-04-22 07:40:39 --> 404 Page Not Found: Accout/index
+ERROR - 2021-04-22 07:42:09 --> 404 Page Not Found: Account_area/profile
+ERROR - 2021-04-22 07:42:20 --> 404 Page Not Found: account/Profile/assets
+ERROR - 2021-04-22 07:44:26 --> 404 Page Not Found: account/Profile/assets
