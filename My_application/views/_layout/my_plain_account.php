e201d2d888184b073fb609a8d4880fe032b6c066
Madiha <Madiha@manageglobally.io>
2021-04-23 09:27:56 +0500

    tool builder

--- My_application/views/_layout/my_plain_account.php
+++ My_application/views/_layout/my_plain_account.php
@@ -899,12 +899,24 @@ function nextPrev(n) {
   var x = document.getElementsByClassName("tab");
   x[currentTab].style.display = "none";
   currentTab = currentTab + n;
+  document.getElementById("form-send_us").submit();
+  alert("form-send_us");
   if (currentTab >= x.length) {
-    document.getElementById("regForm").submit();
+    document.getElementById("form-send_us").submit();
     return false;
   }
   showTab(currentTab);
 }
+// function nextPrev(n) {
+//   var x = document.getElementsByClassName("tab");
+//   x[currentTab].style.display = "none";
+//   currentTab = currentTab + n;
+//   if (currentTab >= x.length) {
+//     document.getElementById("regForm").submit();
+//     return false;
+//   }
+//   showTab(currentTab);
+// }
 
 function fixStepIndicator(n) {
   var i, x = document.getElementsByClassName("step");
