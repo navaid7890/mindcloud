0d6450aab72ae9cbdf1390c84899ef9955c20e94
talhaMG <talha@manageglobally.io>
2021-05-04 10:15:49 +0500

    finance income tool builder

---
+++ My_application/logs/log-2021-05-04.php
@@ -0,0 +1,9 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-05-04 06:37:33 --> 404 Page Not Found: account/Profile/assets
+ERROR - 2021-05-04 06:37:43 --> 404 Page Not Found: account/Profile/assets
+ERROR - 2021-05-04 06:39:18 --> 404 Page Not Found: account/Profile/assets
+ERROR - 2021-05-04 06:39:42 --> 404 Page Not Found: account/Profile/assets
+ERROR - 2021-05-04 07:11:52 --> 404 Page Not Found: account/Profile/assets
+ERROR - 2021-05-04 07:13:36 --> 404 Page Not Found: account/Profile/assets
+ERROR - 2021-05-04 07:13:51 --> 404 Page Not Found: account/Profile/assets
