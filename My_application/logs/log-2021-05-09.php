68e6fd88bc72cab63dcf15f2968f3ce69eba03c3
talhaMG <talha@manageglobally.io>
2021-05-09 16:31:54 +0500

    test creds

---
+++ My_application/logs/log-2021-05-09.php
@@ -0,0 +1,9 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-05-09 13:30:49 --> 404 Page Not Found: Profile/profile.png
+ERROR - 2021-05-09 13:30:54 --> 404 Page Not Found: admin/Profile/profile.png
+ERROR - 2021-05-09 13:30:58 --> 404 Page Not Found: admin/Profile/profile.png
+ERROR - 2021-05-09 13:31:01 --> 404 Page Not Found: admin/Profile/profile.png
+ERROR - 2021-05-09 13:31:06 --> 404 Page Not Found: admin/Profile/profile.png
+ERROR - 2021-05-09 13:31:09 --> 404 Page Not Found: admin/Profile/profile.png
+ERROR - 2021-05-09 13:31:14 --> 404 Page Not Found: admin/Profile/profile.png
