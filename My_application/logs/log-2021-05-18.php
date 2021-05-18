6008b46bfaf5a77364cbf6608fc5e32f4dd85c01
MFazalMamoon <82014645+MFazalMamoon@users.noreply.github.com>
2021-05-18 09:56:41 +0500

    updated icons toolbuilder

---
+++ My_application/logs/log-2021-05-18.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-05-18 06:07:27 --> 404 Page Not Found: account/Profile/assets
+ERROR - 2021-05-18 06:16:55 --> 404 Page Not Found: account/Profile/assets
+ERROR - 2021-05-18 06:17:47 --> 404 Page Not Found: account/Profile/assets
+ERROR - 2021-05-18 06:17:56 --> 404 Page Not Found: account/Profile/assets
+ERROR - 2021-05-18 06:20:58 --> 404 Page Not Found: account/Profile/assets
+ERROR - 2021-05-18 06:21:18 --> 404 Page Not Found: account/Profile/assets
+ERROR - 2021-05-18 06:35:59 --> 404 Page Not Found: Front_assets/images
+ERROR - 2021-05-18 06:36:33 --> 404 Page Not Found: Front_assets/images
+ERROR - 2021-05-18 06:48:00 --> 404 Page Not Found: account/Profile/assets
+ERROR - 2021-05-18 06:50:29 --> 404 Page Not Found: account/Profile/assets
