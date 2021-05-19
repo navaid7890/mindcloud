8b57bfc311c134430672532abf0fc6e7fc88cf68
Madiha <Madiha@manageglobally.io>
2021-05-19 09:37:01 +0500

    Quiz file

--- My_application/controllers/Quiz.php
+++ My_application/controllers/Quiz.php
@@ -252,4 +252,50 @@ class Quiz extends MY_Controller {
 
     }
 
+
+    // public function test()
+    // {
+    //   $this->load->library('excel');
+
+    //   $object = new PHPExcel();
+
+    //   $object->setActiveSheetIndex(0);
+    
+    //   $table_columns = array("Name", "Address", "Gender", "Designation", "Age");
+    
+    //   $column = 0;
+    
+    //   foreach($table_columns as $field)
+    //   {
+    //    $object->getActiveSheet()->setCellValueByColumnAndRow($column, 1, $field);
+    //    $column++;
+    //   }
+    
+    //   $employee_data = $this->model_tool_builder->find_all_active();
+    
+    //   $excel_row = 2;
+    
+    //   foreach($employee_data as $row)
+    //   {
+    //    $object->getActiveSheet()->setCellValueByColumnAndRow(0, $excel_row, $row->name);
+    //    $object->getActiveSheet()->setCellValueByColumnAndRow(1, $excel_row, $row->address);
+    //    $object->getActiveSheet()->setCellValueByColumnAndRow(2, $excel_row, $row->gender);
+    //    $object->getActiveSheet()->setCellValueByColumnAndRow(3, $excel_row, $row->designation);
+    //    $object->getActiveSheet()->setCellValueByColumnAndRow(4, $excel_row, $row->age);
+    //    $excel_row++;
+    //   }
+    
+    //   $object_writer = PHPExcel_IOFactory::createWriter($object, 'Excel5');
+    //   header('Content-Type: application/vnd.ms-excel');
+    //   header('Content-Disposition: attachment;filename="Employee Data.xls"');
+    //   $object_writer->save('php://output');
+    //  }
+    
+
+
+    
+
+
+  
+
 }
