52880db6e1caebd923b4c0483176ed46142f8976
MFazalMamoon <82014645+MFazalMamoon@users.noreply.github.com>
2021-06-08 16:34:49 +0500

    most watch tutorials

--- My_application/controllers/Home.php
+++ My_application/controllers/Home.php
@@ -1,237 +1,253 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Home extends MY_Controller {
+class Home extends MY_Controller
+{
 
-    /**
-     * Default Controller
-     */
+  /**
+   * Default Controller
+   */
 
-    public function __construct()
-    {
-        // Call the Model constructor latest_product
-        $this->cms_page_id = 2;
-        parent::__construct();
+  public function __construct()
+  {
+    // Call the Model constructor latest_product
+    $this->cms_page_id = 2;
+    parent::__construct();
 
-        //$this->login_redirect_check_front();
-        
-        //$this->plugin_seo();
-    }
+    //$this->login_redirect_check_front();
 
-    // Default Home Page
-    public function index()
-    {
-        global $config;
-        $this->cms_page_id = 2;
-        
-        $this->layout_data['title'] = g('db.admin.site_title');
-     
+    //$this->plugin_seo();
+  }
 
-        //BANNER
-        $data['banner'] = $this->model_banner->get_banners();
-       
+  // Default Home Page
+  public function index()
+  {
+    global $config;
+    $this->cms_page_id = 2;
 
-        $data['professions'] = $this->model_profession->find_all_active();            
-        $data['states'] = $this->model_states->find_all_active();            
-        
-        $param = array();
-        $param['where']['tutorial_featured'] = 1;
-        $data['course'] = $this->model_tutorial->find_all_active($param);
+    $this->layout_data['title'] = g('db.admin.site_title');
 
-        
-        $cont = $this->model_cms_page->get_page(2);
-        // debug($cont);
-        $data['cont1'] = $cont['child'][0];
-        $data['cont2'] = $cont['child'][1];
-        $data['cont3'] = $cont['child'][2];
-        $data['cont4'] = $cont['child'][3];
-        $data['cont5'] = $cont['child'][4];
-        $data['cont6'] = $cont['child'][5];
-        $data['cont7'] = $cont['child'][6];
-        $data['cont8'] = $cont['child'][7];
-        $data['cont9'] = $cont['child'][8];
-        $data['cont10'] = $cont['child'][9];
-        $data['cont11'] = $cont['child'][10];
-        $data['cont12'] = $cont['child'][11];
-        $data['cont13'] = $cont['child'][12];
-        $data['cont14'] = $cont['child'][13];
-        $data['cont15'] = $cont['child'][14];
 
-        $data['banner'] = $this->model_inner_banner->find_by_pk(1); 
+    //BANNER
+    $data['banner'] = $this->model_banner->get_banners();
 
-        $data['learning'] = $this->model_learning->find_all_active();
-        $data['testimonial'] = $this->model_testimonials->find_all_active();
+
+    $data['professions'] = $this->model_profession->find_all_active();
+    $data['states'] = $this->model_states->find_all_active();
+
+    $param = array();
+    $param['where']['tutorial_featured'] = 1;
+    $data['course'] = $this->model_tutorial->find_all_active($param);
+
+
+    $cont = $this->model_cms_page->get_page(2);
+    // debug($cont);
+    $data['cont1'] = $cont['child'][0];
+    $data['cont2'] = $cont['child'][1];
+    $data['cont3'] = $cont['child'][2];
+    $data['cont4'] = $cont['child'][3];
+    $data['cont5'] = $cont['child'][4];
+    $data['cont6'] = $cont['child'][5];
+    $data['cont7'] = $cont['child'][6];
+    $data['cont8'] = $cont['child'][7];
+    $data['cont9'] = $cont['child'][8];
+    $data['cont10'] = $cont['child'][9];
+    $data['cont11'] = $cont['child'][10];
+    $data['cont12'] = $cont['child'][11];
+    $data['cont13'] = $cont['child'][12];
+    $data['cont14'] = $cont['child'][13];
+    $data['cont15'] = $cont['child'][14];
+
+    $data['banner'] = $this->model_inner_banner->find_by_pk(1);
+
+    $data['learning'] = $this->model_learning->find_all_active();
+    $data['testimonial'] = $this->model_testimonials->find_all_active();
+
+    $param = array();
+    $param['where']['category_featured'] = 1;
+    $data['category'] = $this->model_category->find_all_active($param);
+
+
+    $data['testi'] = $this->model_testimonials->find_all_active();
+    $firststate = $this->model_states->find_one_active();
+    $data['firststate'] = $firststate['states_id'];
+
+    $param = array();
+    $param['order'] = "videos_views DESC";
+    $param['limit'] = 3;
+
+    $videos = $this->model_videos->find_all_active($param);
+    
+		foreach ($videos as $key => $value) {
+			$all[] = $value['videos_id'];
+		}
+    
+    $param = array();
+    $param['where_in']['cp_tutorial_id'] = $all;
+    $tut = $this->model_course_tutorial->find_all_active($param);
+
+    foreach ($tut as $key => $value) {
+			$all1[] = $value['cp_course_id'];
+		}
+    
+    $param = array();
+    $param['where_in']['tutorial_id'] = $all1;
+    $data['tut'] = $this->model_tutorial->find_all_active($param);
+    // debug($data['tut']);
    
-        $param = array();
-        $param['where']['category_featured'] = 1;
-        $data['category'] = $this->model_category->find_all_active($param); 
+    // $param = array();
+    // $param['where']['cp_tutorial_id'] = 1;
+    // $data['category'] = $this->model_category->find_all_active($param);
 
 
-      $data['testi'] = $this->model_testimonials->find_all_active();
-      $firststate = $this->model_states->find_one_active();            
-      $data['firststate'] =$firststate['states_id'];
+    // $data['config']=$config;
+    //debug($data['config']);
+    // Load View
+    $this->load_view("home", $data);
+  }
 
 
-      $param = array();
-      $param['order'] = "videos_views DESC"; 
-      $param['limit'] = 3;
+  //HOME PAGE SEARCH
+  // home page search
+  public function search()
+  {
 
-      $data['videos'] = $this->model_videos->find_all_active($param);
-        
-      // $data['config']=$config;
-      //debug($data['config']);
-        // Load View
-        $this->load_view("home",$data);
+    $this->layout_data['title'] = g('db.admin.site_title') . " | Search";
+
+    // $data['banner'] = $this->model_banner->get_banners();
+
+
+    if (isset($_GET['state']) || !empty($_GET['category']) || !empty($_GET['keyword'])) {
+      $state = trim(htmlentities($_GET['state']));
+      $profession = trim(htmlentities($_GET['category']));
+      $keyword = trim(htmlentities($_GET['keyword']));
+
+      if (!empty($keyword)) {
+        $param['where_like'][] = array('column' => 'tutorial_keywords', 'value' => $keyword, 'type' => 'both');
+      }
+      if (!empty($state)) {
+        $param['where_like'][] = array('column' => 'tutorial_state_id', 'value' => $state, 'type' => 'both');
+      }
+      if (!empty($profession)) {
+        // $param['where_like'][] = array('column'=>'course_state_id','value'=>$state,'type'=>'both');   
+        $param['joins'][] = array(
+          "table" => "tutorial_profession",
+          "joint" => "tutorial_profession.cp_course_id = tutorial.tutorial_id and cp_profession_id = $profession"
+        );
+      }
+      $pro = $this->model_tutorial->find_all_active($param);
+      //  debug($pro);
+      // debug( $this->db->last_query(),1);
+      $data['course'] = $pro;
     }
 
+    $data['searchfor'] = $_GET['search'];
+    $data['in_banner']['inner_banner_title'] = "Search";
 
-      //HOME PAGE SEARCH
-            // home page search
-    public function search()
-    {
+    $data['professions'] = $this->model_profession->find_all_active();
+    $data['states'] = $this->model_states->find_all_active();
 
-      $this->layout_data['title'] = g('db.admin.site_title')." | Search";
-      
-        // $data['banner'] = $this->model_banner->get_banners();
-          
+    // unset($_POST);
+    // echo json_encode(array("data"=>$this->load->view('shop/filter_brand', $data, true)));
+    // debug($data['pro']);
+    // Load View
+    $data['state'] = $state;
+    $data['profession'] = $profession;
+    $data['keyword'] = $keyword;
+    // debug($state);
+    // debug($profession);
+    // debug($keyword);
 
-        if(isset($_GET['state']) || !empty($_GET['category']) || !empty($_GET['keyword']))
-        {
-            $state = trim(htmlentities($_GET['state']));
-            $profession = trim(htmlentities($_GET['category']));
-            $keyword = trim(htmlentities($_GET['keyword']));
-        
-        if (!empty($keyword)) {
-        $param['where_like'][] = array('column'=>'tutorial_keywords','value'=>$keyword,'type'=>'both');   
-        }
-        if (!empty($state)) {
-        $param['where_like'][] = array('column'=>'tutorial_state_id','value'=>$state,'type'=>'both');   
-        }
-        if (!empty($profession)) {
-        // $param['where_like'][] = array('column'=>'course_state_id','value'=>$state,'type'=>'both');   
-       $param['joins'][] = array(
-                            "table"=>"tutorial_profession", 
-                            "joint"=>"tutorial_profession.cp_course_id = tutorial.tutorial_id and cp_profession_id = $profession"
-                                );
-        }
-        $pro = $this->model_tutorial->find_all_active($param);
-       //  debug($pro);
-       // debug( $this->db->last_query(),1);
-          $data['course'] = $pro;
-        }
-
-      $data['searchfor'] = $_GET['search'];
-      $data['in_banner']['inner_banner_title'] = "Search";
-
-        $data['professions'] = $this->model_profession->find_all_active();            
-        $data['states'] = $this->model_states->find_all_active(); 
-        
-        // unset($_POST);
-        // echo json_encode(array("data"=>$this->load->view('shop/filter_brand', $data, true)));
-         // debug($data['pro']);
-             // Load View
-      $data['state'] = $state;
-      $data['profession'] = $profession;
-      $data['keyword'] = $keyword;
-      // debug($state);
-      // debug($profession);
-      // debug($keyword);
-
-        $this->load_view("search", $data);
-      }
+    $this->load_view("search", $data);
+  }
 
 
-    // public function product($slug="")
-    // {
-    //     global $config;
-        
-    //       //TAB TITLE
-    //     $method_title = ucwords($this->uri->segment(1));
-    //     $this->layout_data['title'] = g('db.admin.site_title')." | ".$method_title;
+  // public function product($slug="")
+  // {
+  //     global $config;
 
-    //     //INNER BANNER
-    //      $b = $this->get_ibanner(1);
-    //      $data['ititle'] = $b['ititle'];
-    //      $data['ibanner_img'] = $b['ibanner_img'];
-        
-    //     // // $header_links = array("snaps","sew-buttons");
-    //     // $header_links[0] = "snaps";
-    //     // $header_links[1] = "sew-buttons";
-    //      $view = $slug ;
+  //       //TAB TITLE
+  //     $method_title = ucwords($this->uri->segment(1));
+  //     $this->layout_data['title'] = g('db.admin.site_title')." | ".$method_title;
 
-    //     // Load View
-    //     $this->load_view($view,$data);
-    //     // $this->load_view("snap",$data);
-    // }
+  //     //INNER BANNER
+  //      $b = $this->get_ibanner(1);
+  //      $data['ititle'] = $b['ititle'];
+  //      $data['ibanner_img'] = $b['ibanner_img'];
 
+  //     // // $header_links = array("snaps","sew-buttons");
+  //     // $header_links[0] = "snaps";
+  //     // $header_links[1] = "sew-buttons";
+  //      $view = $slug ;
 
-//     public function va()
-//     {
-//         $user_id = 3;
-//         $this->model_email->notification_register($user_id , 'user');
-//         die();
-
-//         $this->model_user_info->update_package_event($this->userid);
-        
+  //     // Load View
+  //     $this->load_view($view,$data);
+  //     // $this->load_view("snap",$data);
+  // }
 
 
-//         // $dir    = 'docs\html';
-//         // $files1 = scandir($dir);
-//         // $files2 = scandir($dir, 1);
+  //     public function va()
+  //     {
+  //         $user_id = 3;
+  //         $this->model_email->notification_register($user_id , 'user');
+  //         die();
 
-//         // debug($files1);
-//         // debug($files2);
-        
-//         die();
-//         $order_id = $id = 10;
-//         $this->products_rewards($order_id);
-        
-//         die();
-//         $data = $this->model_shop_order->find_by_pk($id);
-//         debug(unserialize($data['order_payment_post']),1);
-
-//         die();
-//         $this->save_result($course_id);
-//         die();
-
-//         $id = 2;
-//         $this->save_result($id);
-
-//         die();
-//         // $user_id = 4;
-//         // $this->model_email->notification_register($user_id , 'admin');
-//         // die();
-//         // Create REST request URL
-// $url = "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=&q=https://news.google.com/news/rss/?ned=us&gl=US&hl=en";
-// // Make request and get resulting JSON response
-// $json_feed = file_get_contents($url);
-// // Dump it to the screen so we can see it
-// var_dump($json_feed);
+  //         $this->model_user_info->update_package_event($this->userid);
 
 
-// die();
-//         for ($i=0; $i <= 22; $i++) { 
-//             $year[] = DATE("Y")-$i;
-//         }
-//         debug($year,1);
-//         $id = 9;
-//         $this->model_email->job_created_notification($id);
-//         //$this->model_email->factory_supplier_created($id,'user',"admin123");
 
-//     }
+  //         // $dir    = 'docs\html';
+  //         // $files1 = scandir($dir);
+  //         // $files2 = scandir($dir, 1);
 
-    // public function test()
-    // {
-    //     $this->model_email->notification_invoice(11 , 'USER');
+  //         // debug($files1);
+  //         // debug($files2);
 
-    // }
+  //         die();
+  //         $order_id = $id = 10;
+  //         $this->products_rewards($order_id);
 
-      public function notfound()
-      {
-        $this->load_view("notfound",$data); 
-      }
+  //         die();
+  //         $data = $this->model_shop_order->find_by_pk($id);
+  //         debug(unserialize($data['order_payment_post']),1);
 
+  //         die();
+  //         $this->save_result($course_id);
+  //         die();
+
+  //         $id = 2;
+  //         $this->save_result($id);
+
+  //         die();
+  //         // $user_id = 4;
+  //         // $this->model_email->notification_register($user_id , 'admin');
+  //         // die();
+  //         // Create REST request URL
+  // $url = "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=&q=https://news.google.com/news/rss/?ned=us&gl=US&hl=en";
+  // // Make request and get resulting JSON response
+  // $json_feed = file_get_contents($url);
+  // // Dump it to the screen so we can see it
+  // var_dump($json_feed);
+
+
+  // die();
+  //         for ($i=0; $i <= 22; $i++) { 
+  //             $year[] = DATE("Y")-$i;
+  //         }
+  //         debug($year,1);
+  //         $id = 9;
+  //         $this->model_email->job_created_notification($id);
+  //         //$this->model_email->factory_supplier_created($id,'user',"admin123");
+
+  //     }
+
+  // public function test()
+  // {
+  //     $this->model_email->notification_invoice(11 , 'USER');
+
+  // }
+
+  public function notfound()
+  {
+    $this->load_view("notfound", $data);
+  }
 }
-
-
-
-
--- My_application/views/widgets/_most_watched_tutorials.php
+++ My_application/views/widgets/_most_watched_tutorials.php
@@ -1,101 +1,86 @@
-<div class="whoHead">
-    <h2>Most watched  <strong> Tutorials</strong></h2>
-    <div class="space"><br></div>
-</div>
-<div class="space"><br><br><br></div>
+<?php if (isset($tut) && array_filled($tut)) : ?>
 
-<ul class="who-list tut-list pb-100">
-<li>
-        <div class="vid-box">
-            <a href="https://www.youtube.com/watch?v=XIMLoLxmTDw" data-fancybox="media">
-            <div class="video-box">
-                <img src="<?=i('')?>whoSec/1.png">
-                <span><i class="fas fa-play"></i>  <p>Preview Course</p> </span>
-            </div>
-            </a>
-            <a href="#" class="cate-tag">business model</a>
 
-            <div class="vid-content">
-            <h4>Building the Right<br> Team</h4>
-            <div class="row align-items-center pt-70">
-                <div class="col-md-9">
-                    <ul class="rating">
-                        <li><img src="<?=i('')?>icons/rat-d.svg"></li>
-                        <li><img src="<?=i('')?>icons/rat-d.svg"></li>
-                        <li><img src="<?=i('')?>icons/rat-d.svg"></li>
-                        <li><img src="<?=i('')?>icons/rat-d.svg"></li>
-                        <li><img src="<?=i('')?>icons/rat-l.svg"></li>
-                    </ul>
+
+
+
+
+    <div class="whoHead">
+        <h2>Most watched <strong> Tutorials</strong></h2>
+        <div class="space"><br></div>
+    </div>
+    <div class="space"><br><br><br></div>
+
+    <ul class="who-list tut-list pb-100">
+        <?php foreach ($tut as $key => $value) : ?>
+            <li>
+                <div class="vid-box">
+                    <a href="<?= g('db.admin.bucket') . $value['tutorial_video'] ?>" data-fancybox="media">
+                        <div class="video-box">
+                            <img src="<?= g('db.admin.bucketimg') . $value['tutorial_image2'] ?>">
+                            <span><i class="fas fa-play"></i> </span>
+                        </div>
+                    </a>
+                    <?
+                    // debug($value);
+                    $coursecat = array();
+                    $coursecat['where']['cp_course_id'] = $value['tutorial_id'];
+                    $cate = $this->model_course_category->find_all_active($coursecat);
+
+                    $cat_name = array();
+                    $cat_name['where']['category_id'] = $cate[0]['cp_category_id'];
+                    $cat_name = $this->model_category->find_all_active($cat_name);
+                    //  debug($cat_name);
+                    ?>
+                    <a href="javascript:;" class="cate-tag"><?= $cat_name[0]['category_name'] ?></a>
+                    <!-- <a href="javascript:;" class="cate-tag"><? //= $value['tutorial_level'] 
+                                                                    ?></a> -->
+                    <div class="vid-content">
+
+
+                        <div class="row align-items-center ">
+                            <div class="col-md-12">
+                                <a href="<?= l('course-detail-expert') . '/' . $value['tutorial_slug'] ?>" style="color:#33415C;">
+                                    <h4><?= $value['tutorial_name'] ?></h4>
+                                </a>
+                                <ul class="rating">
+                                    <?php
+                                    for ($x = 1; $x <= $value['tutorial_rating']; $x++) { ?>
+                                        "
+                                        <li><img src="<?= i('') ?>icons/rat-l.svg"></li>
+                                        ";
+                                    <? } ?>
+                                </ul>
+                            </div>
+                            <!-- <div class="col-md-4 text-right">
+                                                                                <h6><? //= price($value['tutorial_price']) 
+                                                                                    ?></h6>
+                                                                            </div> -->
+                        </div>
+                        <div class="space"><br><br></div>
+                        <div class="row align-items-center">
+                            <div class="col-md-10">
+                                <?
+                                $exp_name = array();
+                                $exp_name['where']['expert_id'] = $value['tutorial_expert_id'];
+                                $exp_name = $this->model_expert->find_all_active($exp_name);
+
+                                // debug($exp_name);
+                                // debug($exp_name['expert_image']);
+
+
+                                ?>
+                                <ul class="login-btn">
+                                    <li class="first last"><a href="javascript:;"><span><img src="<?= g('db.admin.bucketimg') . $exp_name[0]['expert_image'] ?>"></span> <?= $exp_name[0]['expert_name'] ?> <i class="fal fa-angle-down"></i></a></li>
+                                </ul>
+                            </div>
+                            <div class="col-md-2">
+                                <a href="javascript:;" class="arrow-links"><i class="far fa-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-                <div class="col-md-3 text-right">
-                    <h6>$15</h6>      
-                </div>
-            </div>
-            </div>
-        </div>
-    </li>
-
-    <li>
-        <div class="vid-box">
-            <a href="https://www.youtube.com/watch?v=XIMLoLxmTDw" data-fancybox="media">
-            <div class="video-box">
-                <img src="<?=i('')?>whoSec/2.png">
-                <span><i class="fas fa-play"></i>  <p>Preview Course</p> </span>
-            </div>
-            </a>
-            <a href="#" class="cate-tag">business model</a>
-
-            <div class="vid-content">
-            <h4>Business Protection for Entrepreneurs </h4>
-            <div class="row align-items-center pt-70">
-                <div class="col-md-9">
-                    <ul class="rating">
-                        <li><img src="<?=i('')?>icons/rat-d.svg"></li>
-                        <li><img src="<?=i('')?>icons/rat-d.svg"></li>
-                        <li><img src="<?=i('')?>icons/rat-d.svg"></li>
-                        <li><img src="<?=i('')?>icons/rat-d.svg"></li>
-                        <li><img src="<?=i('')?>icons/rat-l.svg"></li>
-                    </ul>
-                </div>
-
-                <div class="col-md-3 text-right">
-                    <h6>$15</h6>      
-                </div>
-            </div>
-            </div>
-        </div>
-    </li>
-
-    <li>
-        <div class="vid-box">
-            <a href="https://www.youtube.com/watch?v=XIMLoLxmTDw" data-fancybox="media">
-            <div class="video-box">
-                <img src="<?=i('')?>whoSec/3.png">
-                <span><i class="fas fa-play"></i> <p>Preview Course</p></span>
-            </div>
-            </a>
-            <a href="#" class="cate-tag">business model</a>
-
-            <div class="vid-content">
-            <h4>Basics of Brands and Branding</h4>
-
-            <div class="row align-items-center pt-70">
-                <div class="col-md-9">
-                    <ul class="rating">
-                        <li><img src="<?=i('')?>icons/rat-d.svg"></li>
-                        <li><img src="<?=i('')?>icons/rat-d.svg"></li>
-                        <li><img src="<?=i('')?>icons/rat-d.svg"></li>
-                        <li><img src="<?=i('')?>icons/rat-d.svg"></li>
-                        <li><img src="<?=i('')?>icons/rat-l.svg"></li>
-                    </ul>
-                </div>
-
-                <div class="col-md-3 text-right">
-                    <h6>$15</h6>      
-                </div>
-            </div>
-            </div>
-        </div>
-    </li>
-</ul>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif ?>
