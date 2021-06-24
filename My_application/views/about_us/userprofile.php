f35e41accaeb71763689bf7eebbe1004263395b7
MFazalMamoon <82014645+MFazalMamoon@users.noreply.github.com>
2021-06-24 12:25:13 +0500

    update members

--- My_application/views/about_us/userprofile.php
+++ My_application/views/about_us/userprofile.php
@@ -24,9 +24,8 @@
         margin-top: 30px;
     }
 </style>
-<?=debug($profile)?>
 
-<!-- $profile['user_firstname'] -->
+<!-- $profile['user_username'] -->
 <section class="myjoin">
     <div class="container">
         <div class="row">
@@ -36,12 +35,12 @@
                         <div class="row">
                             <div class="col-lg-2 col-md-3 col-12">
                                 <div class="ExpertImg">
-                                    <img src="http://localhost/cloud/assets/uploads/user/profile.png" class="img-fluid">
+                                    <img src="<?= get_image($profile['ui_profile_image'], $profile['ui_profile_image_path']) ?>" class="img-fluid">
                                 </div>
                             </div>
                             <div class="col-lg-10 col-md-9 col-12">
                                 <div class="ExpertTextArea">
-                                    <h1>Genny Ghanimeh</h1>
+                                    <h1><?= $profile['user_firstname'] ?> <?= $profile['user_lastname'] ?></h1>
 
                                 </div>
                             </div>
@@ -52,9 +51,9 @@
                             <div class="col-lg-12">
                                 <ul class="nav nav-tabs">
                                     <li class="active"><a data-toggle="tab" class="topTabs" href="#Profile">Profile</a></li>
-                                    <li><a data-toggle="tab" class="topTabs" href="#menu1">Timeline</a></li>
-                                    <li><a data-toggle="tab" class="topTabs" href="#menu2">Connections</a></li>
-                                    <li><a data-toggle="tab" href="#menu3">Group</a></li>
+                                    <li><a data-toggle="tab" class="topTabs" href="#Timeline">Timeline</a></li>
+                                    <li><a data-toggle="tab" class="topTabs" href="#Connections">Connections</a></li>
+                                    <li><a data-toggle="tab" href="#Group">Group</a></li>
                                 </ul>
                             </div>
                         </div>
@@ -65,36 +64,60 @@
         </div>
         <div class="row ">
             <div class="col-lg-12">
-
-
                 <div class="tab-content">
                     <div id="Profile" class="tab-pane fade in active show card">
                         <div class="card-header">View Profile</div>
                         <div class="card-body">
+                            <h2>Experience</h2>
+                            <hr />
+                            <div class="row">
+                                <div class="col-lg-3 col-md-4 col-6">
+                                    <h3>Title</h3>
+                                </div>
+                                <div class="col-lg-9 col-md-8 col-6">
+                                    <h3><?= $profile['ui_exp_title'] ?></h3>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-3 col-md-4 col-6">
+                                    <h3>Company</h3>
+                                </div>
+                                <div class="col-lg-9 col-md-8 col-6">
+                                    <h3><?= $profile['ui_exp_company'] ?></h3>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-3 col-md-4 col-6">
+                                    <h3>Description</h3>
+                                </div>
+                                <div class="col-lg-9 col-md-8 col-6">
+                                    <h3> <?= $profile['ui_exp_desc'] ?></h3>
+                                </div>
+                            </div>
                             <h2>Details</h2>
                             <hr />
                             <div class="row">
                                 <div class="col-lg-3 col-md-4 col-6">
                                     <h3>First Name</h3>
                                 </div>
-                                <div class="col-lg-3 col-md-4 col-6">
-                                    <h3><?=$profile['user_firstname']?></h3>
+                                <div class="col-lg-9 col-md-8 col-6">
+                                    <h3><?= $profile['user_firstname'] ?></h3>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-lg-3 col-md-4 col-6">
                                     <h3>Last Name</h3>
                                 </div>
-                                <div class="col-lg-3 col-md-4 col-6">
-                                    <h3> Ghanimeh</h3>
+                                <div class="col-lg-9 col-md-8 col-6">
+                                    <h3> <?= $profile['user_lastname'] ?></h3>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-lg-3 col-md-4 col-6">
                                     <h3>User Name</h3>
                                 </div>
-                                <div class="col-lg-3 col-md-4 col-6">
-                                    <h3>Genny </h3>
+                                <div class="col-lg-9 col-md-8 col-6">
+                                    <h3><?= $profile['user_username'] ?> </h3>
                                 </div>
                             </div>
                             <h2>Education</h2>
@@ -103,60 +126,36 @@
                                 <div class="col-lg-3 col-md-4 col-6">
                                     <h3>School</h3>
                                 </div>
-                                <div class="col-lg-3 col-md-4 col-6">
-                                    <h3>School </h3>
+                                <div class="col-lg-9 col-md-8 col-6">
+                                    <h3><?= $profile['ui_exp_school'] ?> </h3>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-lg-3 col-md-4 col-6">
                                     <h3>Degree</h3>
                                 </div>
-                                <div class="col-lg-3 col-md-4 col-6">
-                                    <h3> Degree</h3>
+                                <div class="col-lg-9 col-md-8 col-6">
+                                    <h3> <?= $profile['ui_exp_degree'] ?></h3>
                                 </div>
                             </div>
+                            <!-- $profile['user_username'] -->
 
-                            <h2>Experience</h2>
-                            <hr />
-                            <div class="row">
-                                <div class="col-lg-3 col-md-4 col-6">
-                                    <h3>Title</h3>
-                                </div>
-                                <div class="col-lg-3 col-md-4 col-6">
-                                    <h3>School </h3>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-lg-3 col-md-4 col-6">
-                                    <h3>Company</h3>
-                                </div>
-                                <div class="col-lg-3 col-md-4 col-6">
-                                    <h3> Company</h3>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-lg-3 col-md-4 col-6">
-                                    <h3>Description</h3>
-                                </div>
-                                <div class="col-lg-3 col-md-4 col-6">
-                                    <h3> Description</h3>
-                                </div>
-                            </div>
                         </div>
                     </div>
-                    <div id="menu1" class="tab-pane fade card">
+                    <!-- $profile['user_username'] -->
+                    <div id="Timeline" class="tab-pane fade card">
                         <div class="card-header">View Time Line</div>
                         <div class="card-body">
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                         </div>
                     </div>
-                    <div id="menu2" class="tab-pane fade card">
+                    <div id="Connections" class="tab-pane fade card">
                         <div class="card-header">View Connections</div>
                         <div class="card-body">
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                         </div>
                     </div>
-                    <div id="menu3" class="tab-pane fade card">
+                    <div id="Group" class="tab-pane fade card">
                         <div class="card-header">View Groups</div>
                         <div class="card-body">
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
--- My_application/views/account/account_area_theme/_about_us.php
+++ My_application/views/account/account_area_theme/_about_us.php
@@ -1,31 +1,37 @@
 <style>
-    .joinContent strong {
-        font-weight: 700;
-    }
+  .joinContent strong {
+    font-weight: 700;
+  }
 
-    .consult-left h2 strong span strong,
-    .consult-head strong,
-    .mainBanner strong,
-    .joinContent strong {
-        font-weight: 700;
-    }
+  .consult-left h2 strong span strong,
+  .consult-head strong,
+  .mainBanner strong,
+  .joinContent strong {
+    font-weight: 700;
+  }
 
-    .consult-left h2 strong {
-        font-weight: 400;
-    }
-    .vid-content h4 {
+  .consult-left h2 strong {
+    font-weight: 400;
+  }
+
+  .vid-content h4 {
     min-height: 56px;
-}
+  }
 
-.aboutForm h2 {
+  .aboutForm h2 {
     font-size: 25px;
     color: #202020;
     font-weight: 600;
     text-transform: uppercase;
     padding-bottom: 35px;
-}
+  }
 
-textarea, select, input[type], textarea, select, button {
+  textarea,
+  select,
+  input[type],
+  textarea,
+  select,
+  button {
     background: transparent;
     border: none;
     border-radius: 0px;
@@ -34,10 +40,10 @@ textarea, select, input[type], textarea, select, button {
     border: 1px solid #bfc2c5;
     /* padding: 30px 0; */
     margin-bottom: 30px;
-}
+  }
 
 
-#update-about-btn {
+  #update-about-btn {
     background-color: #e28f26;
     color: #fff;
     padding: 10px 30px;
@@ -46,53 +52,79 @@ textarea, select, input[type], textarea, select, button {
     display: block;
     width: 100%;
     margin-top: 15px;
-}
+  }
+
+  form#update-about-form label {
+    margin-bottom: 10px;
+    color: #888686;
+  }
 </style>
 <div class="index-page">
-    <section class="dashboard">
-        <ul class="dashboard-layout">
-            <li>
-                <div class="front-dashboard">
-                    <a href="#" class="menu-dash-front">MENU<i class="fal fa-bars"></i></a>
-                    <? $this->load->view("widgets/dashboard-menu-box"); ?>
-                </div>
-            </li>
+  <section class="dashboard">
+    <ul class="dashboard-layout">
+      <li>
+        <div class="front-dashboard">
+          <a href="#" class="menu-dash-front">MENU<i class="fal fa-bars"></i></a>
+          <? $this->load->view("widgets/dashboard-menu-box"); ?>
+        </div>
+      </li>
 
-            <li>
-            <div class="col-md-6">
-            <form role="form" class="" id='update-about-form'>
-  <div class="row">
-    <input type="hidden" name="user_info[ui_user_id]" value="<?=$user_data['user_id']?>"> 
-    <div class="col-md-12">
-      <input id="website" name="user_info[ui_website]" value='<?=$user_data['ui_website']?>' type="text" class="form-control" placeholder="Your Website Link" />
-    </div>
-    <div class="col-md-12">
-      <textarea id="description" name="user_info[ui_description]" class="form-control" placeholder="Description"><?=$user_data['ui_description']?></textarea>
-    </div>
-    <div class="col-md-3">
-      <div class="addfile">
-        <!-- <label class="custom-file-upload" style="margin-top: 5px;">
+      <li>
+        <div class="col-md-6">
+          <form role="form" class="" id='update-about-form'>
+            <div class="row">
+              <input type="hidden" name="user_info[ui_user_id]" value="<?= $user_data['user_id'] ?>">
+              <div class="col-md-12">
+                <label>Job Title </label>
+                <input name="user_info[ui_exp_title]" value='<?= $user_data['ui_exp_title'] ?>' type="text" class="form-control" placeholder="Your Title" />
+              </div>
+              <div class="col-md-12">
+                <label>Your Company Name </label>
+                <input name="user_info[ui_exp_company]" value='<?= $user_data['ui_exp_company'] ?>' type="text" class="form-control" placeholder="Your Company Name" />
+              </div>
+              <div class="col-md-12">
+                <label>Company Description </label>
+                <textarea name="user_info[ui_exp_desc]" rows="7" class="form-control" placeholder="Your Company Description"><?= $user_data['ui_exp_desc'] ?></textarea>
+              </div>
+              <div class="col-md-12">
+                <label>Website </label>
+                <input id="website" name="user_info[ui_website]" value='<?= $user_data['ui_website'] ?>' type="text" class="form-control" placeholder="Your Website Link" />
+              </div>
+              <div class="col-md-12">
+                <label>Description </label>
+                <textarea id="description" rows="7" name="user_info[ui_description]" class="form-control" placeholder="Description"><?= $user_data['ui_description'] ?></textarea>
+              </div>
+              <div class="col-md-12">
+                <label>School </label>
+                <input name="user_info[ui_exp_school]" value='<?= $user_data['ui_exp_school'] ?>' type="text" class="form-control" placeholder="Your Title" />
+              </div>
+              <div class="col-md-12">
+                <label>Degree </label>
+                <input name="user_info[ui_exp_degree]" value='<?= $user_data['ui_exp_degree'] ?>' type="text" class="form-control" placeholder="Your Title" />
+              </div>
+              <div class="col-md-3">
+                <div class="addfile">
+                  <!-- <label class="custom-file-upload" style="margin-top: 5px;">
           <input type="file" name="file" class="inputfile inputfile-1" style='position: absolute; opacity: 0;padding:5px' />
           Choose a profile image 
           </label> -->
 
 
-          <label class="fileContainer filelabel" style="margin: 10px 0px; border: 1px solid rgb(193, 193, 193);">
-            <img src="<?=get_image($user_data['ui_profile_image'],$user_data['ui_profile_image_path'])?>" class="img-responsive" id='profile-img-tag'  style="display: table;height: 120px;margin: 0 auto;width: 120px;" />
-            <input type="file" id='profile-img' name="file" style="display: none">
-          </label><br />
-          <span style="color:green">Recommendation : 120px x 120px</span>
+                  <label class="fileContainer filelabel" style="margin: 10px 0px; border: 1px solid rgb(193, 193, 193);">
+                    <img src="<?= get_image($user_data['ui_profile_image'], $user_data['ui_profile_image_path']) ?>" class="img-responsive" id='profile-img-tag' style="display: table;height: 120px;margin: 0 auto;width: 120px;" />
+                    <input type="file" id='profile-img' name="file" style="display: none">
+                  </label><br />
+                  <span style="color:green">Recommendation : 120px x 120px</span>
 
-      </div>
-    </div>
-  </div>
-  <button type="submit" id='update-about-btn'>Update Info</button>
-</form>
+                </div>
+              </div>
             </div>
+            <button type="submit" id='update-about-btn'>Update Info</button>
+          </form>
+        </div>
 
 
-            </li>
-        </ul>
-    </section>
+      </li>
+    </ul>
+  </section>
 </div>
-
