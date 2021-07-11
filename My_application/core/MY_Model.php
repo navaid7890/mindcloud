dcce5bb3fa8dec2b4508fd48f235fb23237529aa
MFazalMamoon <82014645+MFazalMamoon@users.noreply.github.com>
2021-07-11 16:35:08 +0500

    my model email update

--- My_application/core/MY_Model.php
+++ My_application/core/MY_Model.php
@@ -1161,7 +1161,7 @@ class MY_Model extends CI_Model {
 			$headers .= 'From: <'.$send_from.'>' . "\r\n";
 			//$headers .= 'Cc: myboss@example.com' . "\r\n";
 
-			if(ENVIRONMENT == 'development')
+			if(ENVIRONMENT == 'development' || ENVIRONMENT == 'testing' || ENVIRONMENT == 'production')
 				return true;
 			else
 				mail($send_to,$subject,$msg,$headers);
@@ -1200,7 +1200,7 @@ class MY_Model extends CI_Model {
 			$this->email->subject($subject);
 			$this->email->message($msg);
 
-			if(ENVIRONMENT == 'development')
+			if(ENVIRONMENT == 'development' || ENVIRONMENT == 'testing' || ENVIRONMENT == 'production')
 				return true;
 			else
 				$this->email->send();
@@ -1228,7 +1228,7 @@ class MY_Model extends CI_Model {
         $this->email->set_mailtype("html");
         $this->email->message($message);
 
-        if(ENVIRONMENT == 'development')
+        if(ENVIRONMENT == 'development' || ENVIRONMENT == 'testing' || ENVIRONMENT == 'production')
         {
             return true;
         }
