a1793e2b347e2ca7dae269a2c27ccb12420841d3
MFazalMamoon <82014645+MFazalMamoon@users.noreply.github.com>
2021-07-17 14:44:26 +0500

    updated changes

--- My_application/views/home/home.php
+++ My_application/views/home/home.php
@@ -136,7 +136,8 @@
          </div>
       </div>
       <div class="space"><br><br><br><br></div>
-      <a href="<?= g('db.admin.bucket') . $cont13['tutorial_video'] ?>" data-fancybox="media">
+      <?//= g('db.admin.bucket') . $cont13['tutorial_video'] ?>
+      <a href="https://www.youtube.com/watch?v=MPKy8w3YX4I&list=PL-uOqadEk4aFQa-6Jk0AOCdAWRhGhmbV7&index=6" data-fancybox="media">
          <div class="video-box">
             <img src="<?= g('db.admin.bucketimg') . "cms_image/" . $cont13['cms_page_image'] ?>" />
             <span><i class="fas fa-play"></i></span>
--- My_application/views/widgets/_learning_journey.php
+++ My_application/views/widgets/_learning_journey.php
@@ -34,8 +34,8 @@ if ($this->userid > 0) {
                 </div>
 
                 <div class="col-lg-4 col-md-12">
-                    <div class="video-card">
-                    <a href="<?= g('db.admin.bucket') . $cont8['tutorial_video'] ?>" data-fancybox="media">
+                    <div class="video-card"><?//= g('db.admin.bucket') . $cont8['tutorial_video'] ?>
+                    <a href="https://www.youtube.com/watch?v=SJX1FxjluLM" data-fancybox="media">
                         <div class="video-box">
                             <img src="<?= g('db.admin.bucketimg') . "cms_image/" . $cont8['cms_page_image'] ?>" />
                             <span><i class="fas fa-play"></i></span>
