7234ae43326ad0b61f708ec349ffb62a87ca6423
MFazalMamoon <82014645+MFazalMamoon@users.noreply.github.com>
2021-07-26 16:38:27 +0500

    updated footer and header

--- My_application/views/_layout/front/_footer.php
+++ My_application/views/_layout/front/_footer.php
@@ -34,6 +34,7 @@ $footer = $this->model_config->find_all_active($footer);
 								<li><a href="<?= l('enterprise-partners') ?>">Enterprise Partners</a></li>
 								<li><a href="<?= l('about-us') ?>">About Us</a></li>
 								<li><a href="<?= l('innovate') ?>">Competition</a></li>
+								<li><a href="<?= l('newsfeed') ?>">News Feed</a></li>
 								<li><a href="<?= l('faq') ?>">FAQs</a></li>
 								<!-- <li><a href="<?= l('') ?>">Contact Us</a></li> -->
 							</ul>
