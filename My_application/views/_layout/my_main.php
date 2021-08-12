9d86e43823872577a7267bb31350d0c4a8d3c3df
MFazalMamoon <82014645+MFazalMamoon@users.noreply.github.com>
2021-08-12 12:03:19 +0500

    update

--- My_application/views/_layout/my_main.php
+++ My_application/views/_layout/my_main.php
@@ -360,6 +360,9 @@ document.onkeydown = function(e) {
   if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
      return false;
   }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'S'.charCodeAt(0)) {
+     return false;
+  }
   if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
      return false;
   }
