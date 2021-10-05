0dc57264e2ad035bbb6224b82faf6e29ea3f1dc9
MFazalMamoon <82014645+MFazalMamoon@users.noreply.github.com>
2021-10-05 16:55:45 +0500

    updated download issue

--- My_application/views/_layout/my_plain_account.php
+++ My_application/views/_layout/my_plain_account.php
@@ -639,7 +639,7 @@ $my_tools = array(
 
 <!-- BEGIN BODY -->
 
-<body class="overFlowHide customeAccountList business-page" id="top" oncontextmenu="return true">
+<body class="overFlowHide customeAccountList business-page" id="top" oncontextmenu="return false">
 
 
   <div id="overlay">
