56b64dcd6e9723ef2a805e67513efe68d4002d25
MFazalMamoon <82014645+MFazalMamoon@users.noreply.github.com>
2021-11-01 10:05:42 +0500

    updated dashboardd

--- My_application/views/widgets/dashboard-menu-box.php
+++ My_application/views/widgets/dashboard-menu-box.php
@@ -16,13 +16,15 @@
                </a>
             </li>
             <!-- <li>
-               <a href="<?//= l('booking') ?>">
+               <a href="<? //= l('booking') 
+                        ?>">
                   <span><img src="<?= i('') ?>dashboard/menu/Group516.svg"></span>
                   Consult with Experts
                </a>
             </li> -->
             <!-- <li>
-               <a href="<?//= l('consult-advisors') ?>">
+               <a href="<? //= l('consult-advisors') 
+                        ?>">
                   <span><img src="<?= i('') ?>dashboard/menu/Group547.svg"></span>
                   Advisory
                </a>
@@ -53,7 +55,8 @@
 
 
             <!-- <li class="dropdown-toggle">
-               <a href="<?//= l('account/profile/learning') ?>">
+               <a href="<? //= l('account/profile/learning') 
+                        ?>">
                   <span><img src="<?= i('') ?>dashboard/menu/2.svg"></span>
                   My Learning Journey
                </a>
@@ -66,12 +69,18 @@
                      // $startupdata = $this->model_startup->find_all_active($startup);
                      ?>
 
-                     <li><a href="<?//= l('account/profile/startup') ?>"> <span><img src="<?//= i('') ?>dashboard/menu/3.svg"></span> My Startup:
-                           <?// if (!empty($startupdata)) : ?>
-                              <?//= $startupdata[0]['startup_name'] ?>
-                           <?// else : ?>
+                     <li><a href="<? //= l('account/profile/startup') 
+                                    ?>"> <span><img src="<? //= i('') 
+                                                                                          ?>dashboard/menu/3.svg"></span> My Startup:
+                           <? // if (!empty($startupdata)) : 
+                           ?>
+                              <? //= $startupdata[0]['startup_name'] 
+                              ?>
+                           <? // else : 
+                           ?>
                               -- -- --
-                           <?// endif; ?>
+                           <? // endif; 
+                           ?>
                         </a>
                      </li>
                   </ul>
@@ -83,15 +92,15 @@
                   <span><img src="<?= i('') ?>dashboard/menu/user.svg"></span>
                   My Profile
                </a>
-
+<!-- 
                <div class="dashboard-child-links">
-                  <ul>
-                     <li>
+                  <ul> -->
+                     <!-- <li>
                         <a href="<?= l('account-area/change-password') ?>">
                            <span><img src="<?= i('') ?>dashboard/menu/dot.png" style="width: 60%;"></span>
                            Password Change
                         </a>
-                     </li>
+                     </li> -->
                      <!-- <li>
                         <a href="<?= l('account-area/address-info') ?>">
                            <span><img src="<?= i('') ?>dashboard/menu/dot.png" style="width: 60%;"></span>
@@ -99,15 +108,34 @@
                         </a>
                      </li> -->
 
-                     <li>
+                     <!-- <li>
                         <a href="<?= l('account-area/change-profile') ?>">
                            <span><img src="<?= i('') ?>dashboard/menu/dot.png" style="width: 60%;"></span>
                            About My Startup
                         </a>
-                     </li>
-
+                     </li> -->
+<!-- 
                   </ul>
-               </div>
+               </div> -->
+            </li>
+            <li>
+               <a href="<?= l('account-area/change-password') ?>">
+                  <span><img src="<?= i('') ?>dashboard/menu/dot.png" style="width: 60%;"></span>
+                  Password Change
+               </a>
+            </li>
+            <!-- <li>
+                        <a href="<?= l('account-area/address-info') ?>">
+                           <span><img src="<?= i('') ?>dashboard/menu/dot.png" style="width: 60%;"></span>
+                           Address Info
+                        </a>
+                     </li> -->
+
+            <li>
+               <a href="<?= l('account-area/change-profile') ?>">
+                  <span><img src="<?= i('') ?>dashboard/menu/dot.png" style="width: 60%;"></span>
+                  About My Startup
+               </a>
             </li>
          </ul>
       </div>
