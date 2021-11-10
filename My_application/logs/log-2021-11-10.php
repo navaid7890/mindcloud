321e795f42edcd2092cd0b448c5b5d6339805261
madihaabcdef123 <madihamukhtar111@gmail.com>
2021-11-10 14:56:35 +0500

    Merchant

---
+++ My_application/logs/log-2021-11-10.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-11-10 14:48:05 --> Query error: Table 'ebdb.md_user_info' doesn't exist - Invalid query: INSERT INTO `md_user_info` (`ui_user_id`) VALUES (176)
+ERROR - 2021-11-10 10:54:09 --> 404 Page Not Found: Images/ajax_loader.gif
