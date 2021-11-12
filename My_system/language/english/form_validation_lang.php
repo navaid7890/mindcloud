e7c1877f4c72ee319b1d84960e8f7318ad4e1a60
madihaabcdef123 <madihamukhtar111@gmail.com>
2021-11-12 16:27:13 +0500

    Register Changes

--- My_system/language/english/form_validation_lang.php
+++ My_system/language/english/form_validation_lang.php
@@ -32,6 +32,7 @@
  * @copyright	Copyright (c) 2014 - 2019, British Columbia Institute of Technology (https://bcit.ca/)
  * @license	https://opensource.org/licenses/MIT	MIT License
  * @link	https://codeigniter.com
+ * 
  * @since	Version 1.0.0
  * @filesource
  */
